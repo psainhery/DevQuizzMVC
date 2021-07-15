2a55aebb7dffc7c8f6d0d5b37f128e21addf05b2
Phrador Sainhery <phrador.sainhery@aims-cameroon.org>
2021-07-15 09:32:49 +0200

    LinkCreateUser

---
+++ DevQuizzMVC/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using DevQuizzMVC.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevQuizzMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View(new UserDTO());
+        }
+        [HttpPost]
+        public ActionResult Index(UserDTO userDTO)
+        {
+            return View(userDTO);
+        }
+    }
+    
+}
